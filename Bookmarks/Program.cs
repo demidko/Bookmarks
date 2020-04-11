b66203d1bf04f029dd8e60ffee7d950570d3bd3e
Daniel Demidko <demidko@farpost.com>
2020-04-12 01:54:16 +1000

    project files

---
+++ Bookmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bookmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
